24e55d824c01cc18ebd462f417aa2d468747de75
Hatobus <s1240056@gmail.com>
2017-11-11 03:28:52 +0900

    add

---
+++ hog.sh
@@ -0,0 +1,6 @@
+echo "# Hatoson" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin https://github.com/hatobus/Hatoson.git
+git push -u origin master
---
+++ slackbot_settings.py
@@ -0,0 +1,9 @@
+
+# botアカウントのトークンを指定
+API_TOKEN = "xoxb-269810576608-7dU3yZkVepwLm9T3PTD5dsVK"
+
+# このbot宛のメッセージで、どの応答にも当てはまらない場合の応答文字列
+DEFAULT_REPLY = "ポッポ〜（意味不明）"
+
+# プラグインスクリプトを置いてあるサブディレクトリ名のリスト
+PLUGINS = ['plugins']
